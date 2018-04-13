fea44d5a9e6aae19f3178c6f35d42e0924eb69c5
anddyoleinikov <dyt321@ya.ru>
2018-04-13 15:59:30 +0300

    Initial version

--- README.md
+++ README.md
@@ -1,6 +1,42 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+With this script you can see who submit tasks at night to [DEVMAN.org](https://devman.org).
+
+# Quickstart
+
+Start the script from the command line.
+```bash
+$ python seek_dev_nighters.py
+```
+
+
+### Examples of script launch on Linux, Python 3.5:
+
+```bash
+
+$ python seek_dev_nighters.py
+Users who submit tasks at night:
+jarrymousemouse
+dreamfall3r
+apelsinovv
+paganismrus
+oeananas
+khokhlov86
+id311542249
+av.bikkulova
+IvanKumeyko
+ИванСиницын
+```
+
+# How to Install
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+```bash
+pip install -r requirements.txt # alternatively try pip3
+```
+
+Remember, it is recommended to use [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) for better isolation.
 
 # Project Goals
 
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,56 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import requests
+import sys
+from pytz import timezone
+from datetime import datetime, time
 
-def get_midnighters():
-    pass
+
+def get_json(url):
+    result = requests.get(url)
+    if result.status_code == 200:
+        return result.json()
+    return None
+
+
+def load_attempts(api_url):
+    pages = get_json(api_url)['number_of_pages']
+    for page in range(1, pages+1):
+        page_json = get_json(api_url + ('?page=%s' % page))
+        for record in page_json['records']:
+            yield record
+
+
+def get_midnighters(attempts):
+    midnighters = set()
+    for record in attempts:
+        if midnighter_check(record):
+            midnighters.add(record['username'])
+    return midnighters
+
+
+def midnighter_check(record):
+    user_tz = timezone(record['timezone'])
+    submit_time = user_tz.localize(
+        datetime.fromtimestamp(record['timestamp']),
+        is_dst=None
+    )
+    midnight = user_tz.localize(
+        datetime.combine(submit_time.date(), time(0, 0)),
+        is_dst=None
+    )
+    morning = user_tz.localize(
+        datetime.combine(submit_time.date(), time(6, 0)),
+        is_dst=None
+    )
+    if morning >= submit_time >= midnight:
+        return True
+
 
 if __name__ == '__main__':
-  pass
+    api_url = 'https://devman.org/api/challenges/solution_attempts/'
+    if not get_json(api_url):
+        sys.exit('API url is unavailable')
+    attempts = load_attempts(api_url)
+    midnighters = get_midnighters(attempts)
+    print('Users who submit tasks at night:')
+    for user in midnighters:
+        print(user)
