94ffb15f7abfdbb29518d7781c03c5acea81865f
anddyoleinikov <dyt321@ya.ru>
2018-04-17 11:34:48 +0300

    simplified time comparison, removed additional request

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,53 +1,50 @@
 import requests
 import sys
 from pytz import timezone
-from datetime import datetime, time
+from datetime import datetime
 
 
-def get_json(url):
-    request_result = requests.get(url)
+def get_data_from_json(url, payload=None):
+    request_result = requests.get(url, params=payload)
     if request_result.ok:
         return request_result.json()
-    return None
 
 
 def load_attempts(api_url):
-    pages = get_json(api_url)['number_of_pages']
-    for page in range(1, pages+1):
-        page_json = get_json(api_url + ('?page=%s' % page))
-        for record in page_json['records']:
+    page = 1
+    pages = 1
+    while page <= pages:
+        payload = {'page': page}
+        page_data = get_data_from_json(api_url, payload)
+        pages = page_data['number_of_pages']
+        for record in page_data['records']:
             yield record
+        page += 1
 
 
 def get_midnighters(attempts):
     midnighters = set()
-    for record in attempts:
-        if midnighter_check(record):
-            midnighters.add(record['username'])
+    for attempt in attempts:
+        if is_midnighter(attempt):
+            midnighters.add(attempt['username'])
     return midnighters
 
 
-def midnighter_check(record):
+def is_midnighter(record):
     user_tz = timezone(record['timezone'])
-    submit_time = user_tz.localize(
-        datetime.fromtimestamp(record['timestamp']),
-        is_dst=None
+    submit_time = datetime.fromtimestamp(
+        record['timestamp'],
+        tz=user_tz
     )
-    midnight = user_tz.localize(
-        datetime.combine(submit_time.date(), time(0, 0)),
-        is_dst=None
-    )
-    morning = user_tz.localize(
-        datetime.combine(submit_time.date(), time(6, 0)),
-        is_dst=None
-    )
-    if morning >= submit_time >= midnight:
+    midnight = 0
+    morning = 6
+    if morning >= submit_time.hour >= midnight:
         return True
 
 
 if __name__ == '__main__':
     api_url = 'https://devman.org/api/challenges/solution_attempts/'
-    if not get_json(api_url):
+    if not get_data_from_json(api_url):
         sys.exit('API url is unavailable')
     attempts = load_attempts(api_url)
     midnighters = get_midnighters(attempts)
